f466442537b1924412aac48809ff7631e0fe816b
juvencio <juvlinhares@gmail.com>
2023-06-15 12:17:31 -0300

    projeto criado

---
+++ src/main/java/com/migration/migrationbetweenbanks/MigrationbetweenbanksApplication.java
@@ -0,0 +1,12 @@
+package com.migration.migrationbetweenbanks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigrationbetweenbanksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigrationbetweenbanksApplication.class, args);
+	}
+}
---
+++ src/test/java/com/migration/migrationbetweenbanks/MigrationbetweenbanksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.migration.migrationbetweenbanks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigrationbetweenbanksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
